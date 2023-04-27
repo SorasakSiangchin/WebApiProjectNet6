4ffcd491ede5022e42edad7e855717e8409d4d12
SorasakSiangchin <112231280+SorasakSiangchin@users.noreply.github.com>
2023-04-27 21:36:28 +0700

    ModelCartChange

--- BackEnd/Models/CartCustomer.cs
+++ BackEnd/Models/CartCustomer.cs
@@ -7,12 +7,13 @@ namespace BackEnd.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string ID { get; set; }
-        public string CustomerID { get; set; }
-        public string ProductID { get; set; }
         public int AmountProduct { get; set; } 
         public DateTime Created { get; set; } = DateTime.Now;
+        public string CustomerID { get; set; }
         [ForeignKey("CustomerID")]
         public virtual Customer Customer { get; set; }
+
+        public string ProductID { get; set; }
         [ForeignKey("ProductID")]
         public virtual Product Product { get; set; }
 
